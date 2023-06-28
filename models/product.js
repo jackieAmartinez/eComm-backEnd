c251f68462355f6c57f027bb58f0abc56b87751d
Chester Springwood <jackieAmartinez@gmail.com>
2023-06-28 19:35:58 -0400

    deep breaths are neat

--- models/product.js
+++ models/product.js
@@ -6,38 +6,39 @@ const sequelize = require('../config/connection');
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
+
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+// define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
     },
     product_name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
       validate: {
-        isDecimal: true,
-      },
+        isDecimal: true
+      }
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
       validate: {
-        isNumeric: true,
-      },
+        isNumeric: true
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
-      references:{
+      references: {
         model: 'category',
         key: 'id',
       },
